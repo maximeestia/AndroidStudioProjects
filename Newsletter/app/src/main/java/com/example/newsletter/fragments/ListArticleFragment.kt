5b480a9916160bf645e4bcec97edf8615e1c43d2
Maxime Rivoalen <m.rivoalen@net.estia.fr>
2020-11-20 11:53:03 +0100

    modif fragment

--- Newsletter/app/src/main/java/com/example/newsletter/fragments/ListArticleFragment.kt
+++ Newsletter/app/src/main/java/com/example/newsletter/fragments/ListArticleFragment.kt
@@ -1,9 +1,32 @@
 package com.example.newsletter.fragments
 
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
+import com.example.newsletter.models.Article
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ListArticleFragment: Fragment() {
     private lateinit var recyclerView: RecyclerView
+    /**
+     * Récupère la liste des articles dans un thread secondaire
+     */
+    private fun getArticles() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            val articles = ArticleRepository.getInstance().getArticles()
+        }
+    }
 
+    /**
+     * Rempli le recyclerview avec les données récupérées dans le web service
+     * Cette action doit s'effectuer sur le thread principale
+     * Car on ne peut mas modifier les éléments de vue dans un thread secondaire
+     */
+    private fun bindData(articles: List<Article>) {
+        lifecycleScope.launch(Dispatchers.Main) {
+            //créer l'adapter
+            //associer l'adapteur au recyclerview
+        }
+    }
 }
