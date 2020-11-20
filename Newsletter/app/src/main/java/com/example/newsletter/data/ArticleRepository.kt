b44b22bd60f060955fffca108039de6a3a7665a8
Maxime Rivoalen <m.rivoalen@net.estia.fr>
2020-11-20 14:30:20 +0100

    Classe ArticleRpository completer

--- Newsletter/app/src/main/java/com/example/newsletter/data/ArticleRepository.kt
+++ Newsletter/app/src/main/java/com/example/newsletter/data/ArticleRepository.kt
@@ -1,5 +1,22 @@
 package com.example.newsletter.data
 
+import com.example.newsletter.models.Article
+
 class ArticleRepository {
-    
+    private val apiService: ArticleResponse
+
+    init {
+        apiService = RetrofitApiService
+    }
+
+    fun getArticles(): List<Article> = apiService.articles
+        companion object {
+        private var instance: ArticleRepository? = null
+        fun getInstance(): ArticleRepository {
+            if (instance == null) {
+                instance = ArticleRepository()
+            }
+            return instance!!
+        }
+    }
 }
